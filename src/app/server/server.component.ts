3c910f1938a03fd8e6c0c958f430b8da83bae099
Rajsheel <arajsheel@ets.org>
2018-04-27 11:54:17 -0400

    Added new components

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+ 
+  getServerName() {
+
+   var name = Date();
+
+  	return name ;
+  }
+}
